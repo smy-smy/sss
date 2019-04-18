20f47c53ad4e596a7b5c0b9d4434630e0b88676b
unknown <2363400792@qq.com>
2019-04-18 09:08:48 +0800

    日考

---
+++ rk0418-1/rk0418.php
@@ -0,0 +1,29 @@
+<?php 
+//丑数
+function choushu($num){
+	while ($num%2==0) {
+		# code...
+		$num=$num/2;
+		echo "2";
+	}
+	while ($num%3==0) {
+		# code...
+		$num=$num/3;
+		echo "3";
+	}
+	while ($num%5==0) {
+		# code...
+		$num=$num/5;
+		echo "5";
+	}
+	if ($num==1) {
+		# code...
+		echo "</br>";
+		echo "是丑数";
+	}else{
+		echo "不是丑数";
+	}
+}
+choushu(100);
+
+ ?>
