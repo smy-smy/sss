9510fc18714341a80c7ccfd883f2261079313ef3
unknown <2363400792@qq.com>
2019-04-28 08:54:49 +0800

    日考

---
+++ rk0428/rk0828.php
@@ -0,0 +1,16 @@
+<?php 
+$target = 7;
+$array=[[1,2,8,9],[2,4,9,12],[4,7,10,13],[6,8,11,15]];
+ function fo($target){
+ 	find($target,$array);
+ 	for ($i=0; $i < ; $i++) { 
+ 		if ($array <=10) {
+ 			if ($array) {
+ 				# code...
+ 				return true;
+ 			}
+ 		}
+ 	}
+ }
+
+ ?>
