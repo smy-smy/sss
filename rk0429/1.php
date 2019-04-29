aa5ebbae8b8b741e17b00096d3dd869c9235705f
unknown <2363400792@qq.com>
2019-04-29 08:52:28 +0800

    rikao

---
+++ rk0429/1.php
@@ -0,0 +1,17 @@
+<?php 
+class a{
+	function a($a){
+
+		 $arr_A=[1,3,6,9];
+		 $arr_B=[2,4,5,8,20];
+		 
+		Combine($arr_A,$arr_B);
+		if ($a) {
+			# code...
+			Combine($arr_A,$arr_B);
+			return $a;
+		}
+	}
+}
+
+ ?>
