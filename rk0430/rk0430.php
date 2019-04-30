3adcd189883e291452e820d43f31e5266c2270ab
unknown <2363400792@qq.com>
2019-04-30 08:48:18 +0800

    rk

---
+++ rk0430/rk0430.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * 
+ */
+class ClassName extends AnotherClass
+{
+	$n = 10;
+	function __construct(argument)
+	{
+		echo "1";
+	}
+	Number0f1($n);
+	$n++;
+}
+return Number0f1($n);
+
+
+ ?>
