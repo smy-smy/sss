f43b0501d28cb5a8bb4938e83bcc7ce04eb22412
unknown <2363400792@qq.com>
2019-05-05 08:53:09 +0800

    rk

---
+++ rk0505/rk0505.php
@@ -0,0 +1,18 @@
+<?php 
+
+var $a = [1,2,3,4,5,6,7,8];
+var $b = {'a','b','c','d','e'};
+function Aa($a,$b){
+	var $a;
+	for ($i=0; $i < 9; $i++) { 
+		# code...
+		if ($i = $a) {
+			# code...
+			echo "ok";
+			return $a[$i];
+		}
+
+	}
+}
+
+ ?>
