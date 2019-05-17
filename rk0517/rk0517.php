1d67e12230d0c640af2e0093d47185c16588ca85
unknown <2363400792@qq.com>
2019-05-17 08:46:46 +0800

    rk0517

---
+++ rk0517/rk0517.php
@@ -0,0 +1,18 @@
+<?php 
+var UglyNumber;
+$ugly = [];
+for ($i=0; $i < 1; $i++) { 
+	# code...
+	var Ugly Number;
+	$index = $i;
+	Get UglyNumber_Solution($index);
+	if ($ugly) {
+		# code...
+		return GetUglyNumber_Solution($index);
+	}
+
+}
+
+
+
+ ?>
