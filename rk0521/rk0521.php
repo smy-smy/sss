3665e126eb7172088b4ac19410fe5457c2d298d7
unknown <2363400792@qq.com>
2019-05-21 08:46:49 +0800

    rk0521

---
+++ rk0521/rk0521.php
@@ -0,0 +1,13 @@
+<?php 
+
+FindNumsAppearOnce($array);
+$array = [2,4,3,6,3,2,5,5];
+for ($i=0; $i < 1; $i++) { 
+	# code...
+	if ($array) {
+		# code...
+		return FindNumsAppearOnce($array);
+	}
+}
+
+ ?>
