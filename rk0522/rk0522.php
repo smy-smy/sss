dde2fb2bd4923b23dc033c75955871476ea1ce2b
unknown <2363400792@qq.com>
2019-05-22 08:47:03 +0800

    tk0522

---
+++ rk0522/rk0522.php
@@ -0,0 +1,22 @@
+<?php 
+/*
+软工学院最近来了一个新员工Fish，每天早晨总是会拿着一本英文杂志，写些句子在本子上。同事Cat对Fish写的内容颇感兴趣，有一天他向Fish借来翻看，但却读不懂它的意思。例如，“student. a am I”。后来才意识到，这家伙原来把句子单词的顺序翻转了，正确的句子应该是“I am a student.”。Cat对一一的翻转这些单词顺序可不在行，你能帮助他么？。
+编写一个函数实现上述要求，函数原型为：
+ReverseSentence($str)  $str表示顺序翻转的句子
+  返回正确顺序的句子
+如：$str = “student. a am I”
+ReverseSentence($str) 返回 “I am a student.”
+
+*/
+$str = [];
+$array = 'student. a am I';
+$str = 'student. a am I';
+ReverseSentence($str);
+for ($i=0; $i < 1; $i++) { 
+	# code...
+	if ($arr) {
+		# code...
+		ReverseSentence($str);
+	}
+}
+ ?>
