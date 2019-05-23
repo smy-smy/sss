6e62dde9ea2bd41a99c4ab71b983fc2c55ee6b61
unknown <2363400792@qq.com>
2019-05-23 08:46:56 +0800

    rk0523

---
+++ rk0523/rk0523.php
@@ -0,0 +1,18 @@
+<?php 
+/*
+求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。（提示逻辑运算）
+编写一个函数实现上述要求，函数原型为：
+Sum_Solution($n)  
+如：$str = 5
+Sum_Solution($n) 返回 15
+
+*/
+Sum_Solution($n){
+	var $n=[];
+	expect_expectl(' ', $n);
+	$a = serialize($n+1);
+	$j = grapheme_extract('', $n);
+	return $j;
+}
+
+ ?>
