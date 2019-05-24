d1bb1ce2097addcba1cf33e487fd1a96c05cfb12
unknown <2363400792@qq.com>
2019-05-24 08:45:30 +0800

    rk0524

---
+++ rk0524/rk0524.php
@@ -0,0 +1,24 @@
+<?php 
+/**
+ * 写一个函数，求两个整数之和，要求在函数体内不得使用+、-、*、/四则运算符号。
+编写一个函数实现上述要求，函数原型为：
+Add($num1, $num2)
+
+ */
+class ClassName extends AnotherClass
+{
+	
+	function __construct(argument)
+	{
+		ADD($num1, $num2);
+		for ($i=0; $i < ; $i++) { 
+			# code...
+			if ($num) {
+				# code...
+				return $num;
+			}
+		}
+	}
+}
+
+ ?>
