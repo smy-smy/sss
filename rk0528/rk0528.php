e2ae78e49f9f5d5afd14280eb92e8a40c1475166
unknown <2363400792@qq.com>
2019-05-28 08:50:53 +0800

    rk0528

---
+++ rk0528/rk0528.php
@@ -0,0 +1,32 @@
+<?php 
+/**
+ * 在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+编写一个函数实现上述要求，函数原型为：
+Find($target, $array)  $target为要查找的整数，$array为满足要求的数组
+如： $target = 7 
+     $array=[[1,2,8,9],[2,4,9,12],[4,7,10,13],[6,8,11,15]]
+Find($target,$array) 返回 true
+
+ */
+class ClassName extends AnotherClass
+{
+	
+	function __construct(argument)
+	{
+		# code...
+		$target = 7 
+        $array=[[1,2,8,9],[2,4,9,12],[4,7,10,13],[6,8,11,15]]
+        foreach ($variable as $key => $value) {
+        	# code...
+        	idate(format);
+        	if ($array) {
+        		# code...
+        		return $array;
+        	}
+        }
+
+	}
+}
+
+
+ ?>
