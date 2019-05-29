8c11ccdb390a19c560644c59373ad968bb1b6e2a
unknown <2363400792@qq.com>
2019-05-29 08:45:03 +0800

    rk0529

---
+++ rk0529/rk0529.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * 给定两个从小到大排好序的整型数组，要求编写一个函数实现两个有序数组的合并，并保持从小到大的顺序。（除了count函数外不能使用任何其他函数，不能单独排序）
+编写一个函数实现上述要求，函数原型为：
+Combine($arr_A, $arr_B) 
+如： $arr_A=[1,3,6,9] $arr_B=[2,4,5,8,20]
+     Combine($arr_A,$arr_B) 返回数组 [1,2,3,4,5,6,8,9,20]
+
+ */
+class ClassName extends AnotherClass
+{
+	
+	function __construct(argument)
+	{
+		# code...
+		Combine($arr_A, $arr_B) ;
+		if ($arr_a) {
+			# code...
+			Combine($arr_A,$arr_B);
+			return [1,2,3,4,5,6,8,9,20];
+		}
+	}
+}
+
+ ?>
