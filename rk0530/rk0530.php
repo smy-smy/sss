37f722cf0d73de6d35acf3fc038d3b6b8d1ff72c
unknown <2363400792@qq.com>
2019-05-30 08:41:10 +0800

    rk0530

---
+++ rk0530/rk0530.php
@@ -0,0 +1,31 @@
+<?php 
+/**
+ * 输入一个正整数，输出该数二进制表示中1的个数。
+编写一个函数实现上述要求，函数原型为：
+NumberOf1($n) $n为任意一个正整数 
+如： $n = 10
+     NumberOf1($n) 返回 2 (表示10的二进制表示中包含2个1)
+提示： 10 的二进制表示为 1010  得到结果1的个数是 2
+
+ */
+class ClassName extends AnotherClass
+{
+	
+	function __construct(argument)
+	{
+		# code...
+		NumberOf1($n);
+		for ($i=0; $i < ; $i++) { 
+			# code...if
+			if ($n = 10) {
+				# code...
+				return $n;
+			}
+		}
+	}
+}
+
+
+
+
+ ?>
