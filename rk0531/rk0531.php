8e84458594150e61262ccc68183d56aef1d3f055
unknown <2363400792@qq.com>
2019-05-31 08:50:33 +0800

    rk0531

---
+++ rk0531/rk0531.php
@@ -0,0 +1,29 @@
+<?php 
+/**
+ * 根据已知有序数组，实现二分查找算法（折半查找），输入任意数字，返回该数字在数组中的下标位置。
+要求：编写函数实现上述要求，必须使用两种方法实现，一种递归，一种非递归
+
+ */
+class ClassName extends AnotherClass
+{
+	
+	function __construct(argument)
+	{
+		# code...
+		var $a = [];
+		foreach ($variable as $key => $value) {
+			# code...
+			while ( <= 10) {
+				# code...if
+				if ($b) {
+					# code...
+					return $a;
+				}
+			}
+		}
+	}
+}
+
+
+
+ ?>
