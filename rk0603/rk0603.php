a3c0f03c71e13e7c86e3972f0200fbd5ca9fd995
unknown <2363400792@qq.com>
2019-06-03 08:51:46 +0800

    aaa

---
+++ rk0603/rk0603.php
@@ -0,0 +1,30 @@
+<?php 
+
+/**
+ * 有一个X*Y的网格，小团要在此网格上从左上角到右下角，只能走格点且只能向右或向下走。请设计一个算法，计算小团有多少种走法。给定两个正整数int x,int y，请返回小团的走法数目。
+要求：编写函数实现上述要求
+函数原型为：
+CountSteps($x,$y) 
+例如：当$x=1,$y=1,可以画出图如下
+ 
+得到的结果将是 2 种走法
+
+ */
+class ClassName extends AnotherClass
+{
+	
+	function __construct(argument)
+	{
+		# code...
+		CountSteps($x,$y) ;
+		for ($i=0; $i < ; $i++) { 
+			# code...
+			if (CountSteps($x,$y) ) {
+				# code...
+			}
+		}
+	}
+}
+
+
+ ?>
