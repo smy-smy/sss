48a668f1dc8c13778b5966c07ac3b2ece2b16987
unknown <2363400792@qq.com>
2019-06-13 08:42:26 +0800

    留言

---
+++ liuyan.php
@@ -0,0 +1,21 @@
+<?php 
+
+
+ ?>
+<center>
+ <table bgcolor="#D0D0D0" width="100px" height="100px">
+ 	<tr>
+ 		<td> <center> <img src="image/liu.png"></center></td>
+ 	</tr>
+ 	<tr>
+ 		<td><a href="ll.html"><img src="image/2.png"></a></td>
+
+ 	</tr>
+ 	<tr>
+ 		<td><a href="ll.html"><img src="image/2.png"></a></td>
+ 	</tr>
+ 	<tr>
+ 		<td><center><input type="text" name=""><button value=""><a href="yy.html">添加留言</a></button></center></td>
+ 	</tr>
+ </table>
+ </center>
